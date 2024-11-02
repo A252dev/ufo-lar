fb8cde313de0d9820fdd5544316d8c380c878985
A252dev <a252p@outlook.com>
2024-11-02 21:54:09 +0100

    currency support

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,15 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use \App\Models\UserBalance;
 
 
 class ProfileController extends Controller
 {
+
+    private function getUserBalance()
+    {
+        return UserBalance::where("email", auth()->user()->email)->get();
+    }
+
     public function index()
     {
         return response()->json([
             'status' => 'success',
-            'data' => auth()->user(),
+            'data' => [
+                'user' => auth()->user(),
+                'balance' => $this->getUserBalance(),
+            ],
         ]);
     }
 
